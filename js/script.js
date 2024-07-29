4b1dcf6e8f4771ee84f352f9da53dbbe3bbd0662
rodlez <xavier.rodlez@gmail.com>
2024-07-29 16:29:07 +0200

    Add JS typewriter effect in the Header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-// Add custom JavaScript here
+  
+  
+  /**  Effect to type each character of the text parameter and show in the selected element with the defined speed
+ * @param {string} text Text to type
+ * @param {string} element Id of the Element to replace with the text
+ * @param {int} speed Number in ms to delay between typing characters 
+ * */  
+  
+  function typeWriter(text, element, speed) {
+    
+    for (let i = 0; i < text.length; i++) {
+      setTimeout(() => {
+        document.getElementById(element).innerHTML += text.charAt(i);
+      }, speed * i);      
+    } 
+
+  }
+  
+  document.addEventListener('DOMContentLoaded', typeWriter('Xavier Rodlez','typing-text',150));
